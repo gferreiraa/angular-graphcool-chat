fb80e67522f862e69fddbfe32a773cece6bab391
gferreira <gferreiraa@gmail.com>
2019-03-19 22:37:20 -0300

    Apollo Link Error

--- angular-graphcool-chat/src/app/apollo-config.module.ts
+++ angular-graphcool-chat/src/app/apollo-config.module.ts
@@ -2,7 +2,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { ApolloModule, Apollo } from 'apollo-angular';
+import { ApolloLink } from 'apollo-link';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 @NgModule({
@@ -20,8 +22,22 @@ export class ApolloConfigModule {
     const uri = 'https://api.graph.cool/simple/v1/cjtcaq0c00lfg015002m6bx1a';
     const http = httpLink.create({ uri });
 
+    const linkError = onError(({ graphQLErrors, networkError }) => {
+      if (graphQLErrors) {
+        graphQLErrors.map(({ message, locations, path }) =>
+          console.log(
+            `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+          ),
+        );
+      }
+      if (networkError) { console.log(`[Network error]: ${networkError}`); }
+    });
+
     apollo.create({
-      link: http,
+      link: ApolloLink.from([
+        linkError,
+        http
+      ]),
       cache: new InMemoryCache(),
       connectToDevTools: true
     });
