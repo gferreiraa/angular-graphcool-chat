67df78167f32d3e40a4d178834c0dec7c8852db4
gferreira <gferreiraa@gmail.com>
2019-03-20 20:52:54 -0300

    Core Module config

--- angular-graphcool-chat/src/app/core/core.module.ts
+++ angular-graphcool-chat/src/app/core/core.module.ts
@@ -1,6 +1,26 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { ApolloConfigModule } from './../apollo-config.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
-
+  exports: [
+    BrowserModule,
+    ApolloConfigModule,
+    BrowserAnimationsModule,
+    MatToolbarModule
+  ]
 })
-export class CoreModule { }
+export class CoreModule {
+  constructor(
+    // SkipSelf: Ignora Dependencias que estejam no mesmo nível hierarquico do Core Module
+    @Optional() @SkipSelf() parentModule: CoreModule
+  ) {
+
+    if ( parentModule ) {
+      throw new Error('Core Module is already loaded. Import it in the appModule only.');
+    }
+
+  }
+}
